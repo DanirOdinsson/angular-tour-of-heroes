2631c00a8aa423df0ae8203eca88c40636355489
DanirOdinsson <tec.data.communication@gmail.com>
2020-09-16 14:07:04 +0200

    ClickEventBindingDetailsSection

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero'
 import {HEROES} from '../mock-heroes';
 
 @Component({
@@ -7,13 +8,15 @@ import {HEROES} from '../mock-heroes';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES; 
-
-
-
+  heroes = HEROES;
+  selectedHero: Hero;
+  
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit() {}
+  
+  onSelect(hero: Hero) {this.selectedHero = hero;
+  console.log(this.selectedHero);
+  }
 
 }
-
