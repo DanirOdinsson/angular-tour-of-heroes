69d237b2db93d762e45327998782ca4b8f478fd8
DanirOdinsson <tec.data.communication@gmail.com>
2020-10-06 09:16:28 +0200

    GettingHerobyIdNotFoundError

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -21,10 +21,23 @@ export class HeroService
       private http: HttpClient,
       private messageService: MessageService) { }
 
-  getHeroes(): Observable<Hero[]> {
+  
+    /** Getting Hero from Server */
+    getHeroes(): Observable<Hero[]> {
     
-    return this.http.get<Hero[]>(this.heroesUrl).pipe(tap(_=> this.log('fetched heroes')), catchError(this.handleError<Hero[]>('getHeroes', [])));
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+      return this.http.get<Hero[]>(this.heroesUrl).pipe(tap(_=> this.log('fetched heroes')), catchError(this.handleError<Hero[]>('getHeroes', [])));
+      this.messageService.add('HeroService: fetched heroes');
+      return of(HEROES);
+    
+    /** Getting Hero by id and returns undefined if not being found */
+    getHeroNo404<Data>(id: number)_ Observable<Hero> 
+    {
+      const url = '${this.heroesUrl'}/?id=${id}';
+      return this.hhtp.get<Hero>[]>(url)
+      .pipe(map(heroes => heroes[0]), tap(h=>
+        { const outcome = h ? 'fetched' : 'did not find'; 
+        this.log('${outcome} hero id=${id'); }), 
+        catchError(this.handleError<Hero>('getHero id=${id}')));
+    }
   }
 }
